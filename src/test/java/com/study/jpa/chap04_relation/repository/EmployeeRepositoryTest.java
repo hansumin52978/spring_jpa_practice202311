73715714f35e8f65bfbb00becabefb11e93c384c
hansumin52978 <gkstnals2626@naver.com>
2023-11-21 15:59:02 +0900

    #6. JPA 연관관계 매핑(양방향), FETCH JOIN(N + 1 문제)

--- src/test/java/com/study/jpa/chap04_relation/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/study/jpa/chap04_relation/repository/EmployeeRepositoryTest.java
@@ -2,7 +2,6 @@ package com.study.jpa.chap04_relation.repository;
 
 import com.study.jpa.chap04_relation.entity.Department;
 import com.study.jpa.chap04_relation.entity.Employee;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Transactional
